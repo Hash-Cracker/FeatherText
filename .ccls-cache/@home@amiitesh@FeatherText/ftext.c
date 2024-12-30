7404e83f1b6d8bd6286e925e7ca7ae40d1b4ece5
Hash-Cracker <tspamiitesh@gmail.com>
2024-12-30 11:18:13 +0530

    Disabling raw mode at exit and turning off canonical mode

--- .ccls-cache/@home@amiitesh@FeatherText/ftext.c
+++ .ccls-cache/@home@amiitesh@FeatherText/ftext.c
@@ -1,15 +1,25 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void rawMode(){
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+struct termios orig_termios;
+
+void disableRaw(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRaw(){
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRaw);
+
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(){
-  rawMode();
+  enableRaw();
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
